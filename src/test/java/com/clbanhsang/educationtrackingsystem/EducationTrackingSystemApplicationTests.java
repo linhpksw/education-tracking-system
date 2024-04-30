0274f64342aceb8c01fd111c6bc26b9a83ef566a
linhpksw <linhpksw@gmail.com>
2024-04-30 15:06:21 +0700

    first commit

---
+++ src/test/java/com/clbanhsang/educationtrackingsystem/EducationTrackingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clbanhsang.educationtrackingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EducationTrackingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
